df98bfc5e2033d77f8b06e400610aa3f15e21504
tomohirok8 <50911712+tomohirok8@users.noreply.github.com>
2020-07-09 09:15:45 +0900

    PyTorchでCIFAR-10を分類するCNNとモデルの保存と読み込みのコード（GPUで学習）

--- CNN_PyTorch_byGPU.py
+++ CNN_PyTorch_byGPU.py
@@ -1,75 +1,18 @@
+import os
+os.chdir('D:\\GitHub\\DL6')
+os.getcwd()
 from torchvision.datasets import CIFAR10
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import numpy as np
-import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
+import matplotlib.pyplot as plt
+import torch
 
-
-
-# CIFAR-10の読み込み
-cifar10_data = CIFAR10(root="./data",
-                       train=False,download=True,
-                       transform=transforms.ToTensor())
-cifar10_classes = np.array(["airplane", "automobile", "bird", "cat", "deer",
-                            "dog", "frog", "horse", "ship", "truck"])
-print("データの数:", len(cifar10_data))
-
-n_image = 25  # 表示する画像の数
-cifar10_loader = DataLoader(cifar10_data, batch_size=n_image, shuffle=True)
-dataiter = iter(cifar10_loader)  # イテレータ
-images, labels = dataiter.next()  # 最初のバッチを取り出す
-
-plt.figure(figsize=(10,10))  # 画像の表示サイズ
-for i in range(n_image):
-    plt.subplot(5,5,i+1)
-    plt.imshow(np.transpose(images[i], (1, 2, 0)))  # チャンネルを一番後ろに
-    label = cifar10_classes[labels[i]]
-    plt.title(label)
-    plt.tick_params(labelbottom=False, labelleft=False, bottom=False, left=False)  # ラベルとメモリを非表示に
-
-plt.show()
-
-
-# データ拡張してみる
-'''
-torchvision.transformsを使ってデータ拡張を行います。
-今回は、cifar-10の画像に-30〜30°の回転、および0.8〜1.2倍のリサイズを行います。
-これらの処理は、バッチを取り出す際に元の画像に対してランダムに加えられます。
-'''
-transform = transforms.Compose([transforms.RandomAffine([-30, 30], scale=(0.8, 1.2)),  # 回転とリサイズ
-                                transforms.ToTensor()])
-cifar10_data = CIFAR10(root="./data",
-                       train=False,download=True,
-                       transform=transform)
-cifar10_classes = np.array(["airplane", "automobile", "bird", "cat", "deer",
-                            "dog", "frog", "horse", "ship", "truck"])
-print("データの数:", len(cifar10_data))
-
-n_image = 25  # 表示する画像の数
-cifar10_loader = DataLoader(cifar10_data, batch_size=n_image, shuffle=True)
-dataiter = iter(cifar10_loader)  # イテレータ
-images, labels = dataiter.next()  # 最初のバッチを取り出す
-
-plt.figure(figsize=(10,10))  # 画像の表示サイズ
-for i in range(n_image):
-    plt.subplot(5,5,i+1)
-    plt.imshow(np.transpose(images[i], (1, 2, 0)))  # チャンネルを一番後ろに
-    label = cifar10_classes[labels[i]]
-    plt.title(label)
-    plt.tick_params(labelbottom=False, labelleft=False, bottom=False, left=False)  # ラベルとメモリを非表示に
-
-plt.show()
 
 
 # データの前処理
-'''
-データ拡張として、回転とリサイズ、および左右反転を行います。
-また、学習が効率的になるように入力の平均値を0、標準偏差を1にします（標準化）。
-DataLoaderは、訓練データ、テストデータそれぞれで設定しますが、テストデータにはミニバッチ法を適用しないのでバッチサイズは元データのサンプル数にします。
-'''
 affine = transforms.RandomAffine([-15, 15], scale=(0.8, 1.2))  # 回転とリサイズ
 flip = transforms.RandomHorizontalFlip(p=0.5)  # 左右反転
 normalize = transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))  # 平均値を0、標準偏差を1に
@@ -84,7 +27,6 @@ cifar10_test = CIFAR10("./data", train=False, download=True, transform=transform
 batch_size = 64
 train_loader = DataLoader(cifar10_train, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(cifar10_test, batch_size=len(cifar10_test), shuffle=False)
-
 
 # モデルの構築
 class Net(nn.Module):
@@ -110,8 +52,6 @@ net = Net()
 net.cuda()  # GPU対応
 print(net)
 
-
-# 学習
 # 交差エントロピー誤差関数
 loss_fnc = nn.CrossEntropyLoss()
 
@@ -147,7 +87,6 @@ for i in range(20):  # 20エポック学習
     if i%1 == 0:
         print("Epoch:", i, "Loss_Train:", loss_train, "Loss_Test:", loss_test)
 
-
 # 誤差の推移
 plt.plot(range(len(record_loss_train)), record_loss_train, label="Train")
 plt.plot(range(len(record_loss_test)), record_loss_test, label="Test")
@@ -156,7 +95,6 @@ plt.legend()
 plt.xlabel("Epochs")
 plt.ylabel("Error")
 plt.show()
-
 
 # 正解率
 correct = 0
@@ -169,21 +107,32 @@ for i, (x, t) in enumerate(test_loader):
     total += len(x)
 print("正解率:", str(correct/total*100) + "%")
 
+# モデルの保存
+# state_dict()の表示
+for key in net.state_dict():
+    print(key, ": ", net.state_dict()[key].size())
+print(net.state_dict()["conv1.weight"][0])  # 　パラメータの一部を表示
 
-# 訓練済みのモデルを使った予測
-cifar10_loader = DataLoader(cifar10_test, batch_size=1, shuffle=True)
-dataiter = iter(cifar10_loader)
-images, labels = dataiter.next()  # サンプルを1つだけ取り出す
+# 保存
+torch.save(net.state_dict(), "model_cnn.pth")  
 
-plt.imshow(np.transpose(images[0], (1, 2, 0)))  # チャンネルを一番後ろに
-plt.tick_params(labelbottom=False, labelleft=False, bottom=False, left=False)  # ラベルとメモリを非表示に
-plt.show()
 
-net.eval()  # 評価モード
-x, t = images.cuda(), labels.cuda()  # GPU対応
-y = net(x)
-print("正解:", cifar10_classes[labels[0]],
-      "予測結果:", cifar10_classes[y.argmax().item()])
+# モデルの読み込み
+net_loaded = Net()
+net_loaded.load_state_dict(torch.load("model_cnn.pth", map_location=torch.device("cpu")))  #CPU対応
+net_loaded.eval()  # 評価モード
+
+# state_dict()の表示
+for key in net_loaded.state_dict():
+    print(key, ": ", net.state_dict()[key].size())
+print(net_loaded.state_dict()["conv1.weight"][0])  # 　パラメータの一部を表示
+
+
+
+
+
+
+
 
 
 
